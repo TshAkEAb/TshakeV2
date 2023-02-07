2057463466ee88056946be65e22138bc80346792
TshAkEAb <64094722+TshAkEAb@users.noreply.github.com>
2023-02-07 20:46:00 +0300

    update

--- utlis/tg.py
+++ utlis/tg.py
@@ -75,7 +75,7 @@ def Ckuser(message):
     if response["ok"]:
       return True
     elif response["ok"] == False:
-      kb = InlineKeyboardMarkup([[InlineKeyboardButton("اضغط للاشتراك ⏺", url="t.me/zx_xx")] ])
+      kb = InlineKeyboardMarkup([[InlineKeyboardButton("اضغط للاشتراك ⏺", url="t.me/Hbbbb")] ])
       Bot("sendMessage",{"chat_id":chatID,"text":response["result"],"reply_to_message_id":message.id,"parse_mode":"html","disable_web_page_preview":True,"reply_markup":kb})
       return False
   except Exception as e:
