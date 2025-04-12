afd82f6d164ffa4a7f8d2cd5e6fcb705287490ef
TTeam <64094722+TshAkEAb@users.noreply.github.com>
2025-04-12 13:19:55 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import requests,os
 
 try:
   from config import *
-  os.system('pm2 start bot.py --name {} --interpreter python3.8 --interpreter-args -u'.format(BOT_ID))
+  os.system('pm2 start bot.py --name {} --interpreter python3.10 --interpreter-args -u'.format(BOT_ID))
 except Exception as e:
   API_ID = 793178
   API_HASH = '9f4461079f30757ca0a4c23e14bd523f'
